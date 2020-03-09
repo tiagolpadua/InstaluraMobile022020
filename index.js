ab7cee01dea748e1584a8d7863476cf6541aa158
Tiago Pádua <tiagolpadua@gmail.com>
2020-03-08 21:42:08 -0300

    ex10.1

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {AppRegistry} from 'react-native';
 import Login from './src/screens/Login';
 
-AppRegistry.registerComponent('InstaluraMobile022020', () => Login);
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent('InstaluraMobile022020', () => App);
